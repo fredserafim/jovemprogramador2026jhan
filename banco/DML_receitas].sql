845eee1252d4322ad19fb27b93e6e04eb5edf902
Fredserafim <jhanserafim@ymail.com>
2026-06-22 19:20:11 -0300

    ajuste banco, inclusao de dificuldade e categoria

--- banco/DML_receitas].sql
+++ banco/DML_receitas].sql
@@ -1,38 +1,16 @@
-select * from complexidade;
+
 insert into complexidade (dificuldade)
 values('facil');
 insert into complexidade (dificuldade)
 values('médio');
 insert into complexidade (dificuldade)
 values('dificil');
+insert into categoria (classificacao)
+values('Doce');
+insert into categoria (classificacao)
+values('Salgado');
+insert into categoria (classificacao)
+values('Agridoce');
 
 select * from ferramentas;
 
-insert into ferramentas (utensilios)
-values ('batedeira');
-insert into ferramentas (utensilios)
-values ('liquidificador');
-insert into ferramentas (utensilios)
-values ('forno');
-insert into ferramentas (utensilios)
-values ('fogao');
-insert into ferramentas (utensilios)
-values ('geladeira');
-insert into ferramentas (utensilios)
-values ('fue');
-insert into ferramentas (utensilios)
-values ('tigela');
-insert into ferramentas (utensilios)
-values ('travessa');
-insert into ferramentas (utensilios)
-values ('forma');
-insert into ferramentas (utensilios)
-values ('espatula');
-insert into ferramentas (utensilios)
-values ('papel-aluminio');
-insert into ferramentas (utensilios)
-values ('assadeira');
-insert into ferramentas (utensilios)
-values ('panela');
-
-select * from usuario;
