aaedebc808b0a8d8470d0bd8a3f244b4620cb7ca
Rafael Warmling <rafael62376766@alunos.sc.senac.br>
2026-06-23 20:17:10 -0300

    banco
    cadastreis usuarios e receitas

---
+++ banco/receitas cadastradas.sql
@@ -0,0 +1,23 @@
+INSERT INTO receita (id, titulo, tempo, complexidade_id, porcoes, categoria_id, preparo_id, usuario_id, data_criacao) VALUES
+(1, 'Bolo de Chocolate Simples', 45, 1, 8, 3, 1, 1, '2026-06-23'),
+(2, 'Arroz Branco Soltinho', 20, 1, 4, 2, 2, 1, '2026-06-23'),
+(3, 'Frango Grelhado Rápido', 15, 1, 2, 2, 3, 2, '2026-06-23'),
+(4, 'Puré de Batata Caseiro', 25, 1, 4, 2, 4, 1, '2026-06-23'),
+(5, 'Lasanha de Carne Clássica', 60, 2, 6, 4, 5, 2, '2026-06-23'),
+(6, 'Omelete de Queijo Rápida', 10, 1, 1, 1, 6, 1, '2026-06-23'),
+(7, 'Bacalhau à Brás Tradicional', 30, 2, 4, 2, 7, 2, '2026-06-23'),
+(8, 'Panquecas de Pequeno-Almoço', 15, 1, 3, 3, 8, 1, '2026-06-23'),
+(9, 'Chocolate Quente Cremoso', 15, 1, 2, 3, 9, 2, '2026-06-23'),
+(10, 'Macarrão Alho e Óleo', 15, 1, 2, 4, 10, 1, '2026-06-23'),
+(11, 'Empadão de Frango Dourado', 50, 2, 6, 2, 11, 2, '2026-06-23'),
+(12, 'Salada de Tomate e Mozzarela', 5, 1, 2, 1, 12, 1, '2026-06-23'),
+(13, 'Bolo de Ló Tradicional', 35, 1, 10, 3, 13, 2, '2026-06-23'),
+(14, 'Queijo Grelhado com Azeite', 8, 1, 2, 1, 14, 1, '2026-06-23'),
+(15, 'Sopa de Frango com Batata', 40, 1, 4, 2, 15, 2, '2026-06-23'),
+(16, 'Almôndegas ao Molho', 35, 2, 4, 2, 16, 1, '2026-06-23'),
+(17, 'Batata Rústica no Forno', 40, 1, 4, 1, 17, 2, '2026-06-23'),
+(18, 'Arroz de Tomate Malandrinho', 25, 1, 4, 2, 18, 1, '2026-06-23'),
+(19, 'Frango frito na Manteiga', 20, 1, 3, 2, 19, 2, '2026-06-23'),
+(20, 'Petit Gâteau de Chocolate', 20, 3, 2, 3, 20, 1, '2026-06-23');
+
+select * from receita;
---
+++ banco/usuarios cadastrados.sql
@@ -0,0 +1,16 @@
+-- 10 USUÁRIOS
+INSERT INTO usuario (id, nome, senha) VALUES
+(1, 'Rafael', '123456'),
+(2, 'Mariana', '123456'),
+(3, 'Carlos', '123456'),
+(4, 'Fernanda', '123456'),
+(5, 'Lucas', '123456'),
+(6, 'Juliana', '123456'),
+(7, 'Gabriel', '123456'),
+(8, 'Amanda', '123456'),
+(9, 'Bruno', '123456'),
+(10, 'Camila', '123456');
+select * from usuario;
+
+
+
